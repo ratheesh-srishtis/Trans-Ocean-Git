09ae4a3fb935c9790cf5f29d7f3290d58d5b8381
ratheesh-srishtis <113422448+ratheesh-srishtis@users.noreply.github.com>
2024-10-29 11:05:23 +0530

    Initial commit with changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log("Hello from my React app!");
   return (
     <div className="App">
       <header className="App-header">
