d3496b69bdcd1c87550290e4b5b0809356d83bf4
RISHI GOPAN <74784087+Rishigopan@users.noreply.github.com>
2024-11-26 15:53:27 +0530

    opsdashboard

--- src/pages/Operations/OpsDashboard.js
+++ src/pages/Operations/OpsDashboard.js
@@ -3,11 +3,261 @@ import { useNavigate } from "react-router-dom";
 import "../../css/ops-dashboard.css";
 
 const OpsDashboard = () => {
-  
+  const Group = require("../../assets/images/hugeicons_new-job.png");
+
   return (
     <>
-      <div>
-        operations dashboard sdsd
+<div className="d-flex justify-content-between mt-3">
+<div className="leftside d-flex">
+        <ul className="nav nav-underline gap-3 ">
+          <li className="nav-item nav-item-filter">
+            <a
+              className="nav-link carduppercontent"
+              aria-current="page"
+              
+            >
+              All
+            </a>
+          </li>
+          <li className="nav-item nav-item-filter">
+            <a
+              className="nav-link carduppercontent"
+             
+            >
+              Last 24 Hour
+            </a>
+          </li>
+          
+        </ul>
+
+      </div>
+
+      <div class="d-flex gap-2 rightside">
+
+        <div class=" searchmain">
+          <input
+            type="email"
+            className="form-control searchops"
+            id="exampleFormControlInput1"
+            placeholder="Search"
+          />
+          <i className="bi bi-search searchicon"></i>
+        </div>
+        <div class=" filtermainjobs ">
+          <i class="bi bi-funnel-fill filtericon"></i>
+          <select
+            class="form-select form-select-sm filter"
+            aria-label="Small select example"
+            name="status"
+          >
+            <option value="">Filter By Status</option>
+            <option value="">Customer Approved</option>
+            <option value="">Pending From OPS</option>
+            <option value="">Operations Completed</option>
+
+          </select>
+        </div>
+       
+      </div>
+</div>
+
+      <div className="firsttile">
+        <div className="row  gap-5 d-flex justify-content-center ">
+          <div class=" col-3 shadow p-3 mb-5 bg-body-tertiary rounded">
+            <div class="d-flex justify-content-between">
+              <div>
+                <img src={Group}></img>
+              </div>
+              <div className="dashstatus">
+                Customer Approved
+              </div>
+            </div>
+            <div>
+              <span className="dashhead"> Job ID:</span>
+              <p className="toms">TOMS/OM/24/123</p>
+            </div>
+            <div>
+              <span className="dashhead"> Port Name:</span>
+              <p className="toms">Sohar Port</p>
+            </div>
+            <div>
+              <span className="dashhead"> Vessel Name:</span>
+              <p className="toms">MV Viva Globus</p>
+            </div>
+            <div class="d-flex justify-content-between">
+              <div className="viewdetail">            
+        View Detail >>>
+              </div>
+              <div className="d-flex">
+                <i class="bi bi-pencil-square dashedit"></i>
+                <i class="bi bi-trash-fill dashdelete"></i>
+              </div>
+            </div>
+
+          </div>
+          <div class=" col-3 shadow p-3 mb-5 bg-body-tertiary rounded">
+            <div class="d-flex justify-content-between">
+              <div>
+                <img src={Group}></img>
+              </div>
+              <div className="pending">
+                Pending From Operations
+              </div>
+            </div>
+            <div>
+              <span className="dashhead"> Job ID:</span>
+              <p className="toms">TOMS/OM/24/123</p>
+            </div>
+            <div>
+              <span className="dashhead"> Port Name:</span>
+              <p className="toms">Sohar Port</p>
+            </div>
+            <div>
+              <span className="dashhead"> Vessel Name:</span>
+              <p className="toms">MV Viva Globus</p>
+            </div>
+            <div class="d-flex justify-content-between">
+              <div className="viewdetail">            
+        View Detail >>>
+              </div>
+              <div className="d-flex">
+                <i class="bi bi-pencil-square dashedit"></i>
+                <i class="bi bi-trash-fill dashdelete"></i>
+              </div>
+            </div>
+
+          </div>
+          <div class=" col-3 shadow p-3 mb-5 bg-body-tertiary rounded">
+            <div class="d-flex justify-content-between">
+              <div>
+                <img src={Group}></img>
+              </div>
+              <div className="Operations">
+                Operations Completed
+              </div>
+            </div>
+            <div>
+              <span className="dashhead"> Job ID:</span>
+              <p className="toms">TOMS/OM/24/123</p>
+            </div>
+            <div>
+              <span className="dashhead"> Port Name:</span>
+              <p className="toms">Sohar Port</p>
+            </div>
+            <div>
+              <span className="dashhead"> Vessel Name:</span>
+              <p className="toms">MV Viva Globus</p>
+            </div>
+            <div class="d-flex justify-content-between">
+              <div className="viewdetail">            
+        View Detail >>>
+              </div>
+              <div className="d-flex">
+                <i class="bi bi-pencil-square dashedit"></i>
+                <i class="bi bi-trash-fill dashdelete"></i>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <div className="row  gap-5 d-flex justify-content-center ">
+          <div class=" col-3 shadow p-3 mb-5 bg-body-tertiary rounded">
+            <div class="d-flex justify-content-between">
+              <div>
+                <img src={Group}></img>
+              </div>
+              <div className="dashstatus">
+                Customer Approved
+              </div>
+            </div>
+            <div>
+              <span className="dashhead"> Job ID:</span>
+              <p className="toms">TOMS/OM/24/123</p>
+            </div>
+            <div>
+              <span className="dashhead"> Port Name:</span>
+              <p className="toms">Sohar Port</p>
+            </div>
+            <div>
+              <span className="dashhead"> Vessel Name:</span>
+              <p className="toms">MV Viva Globus</p>
+            </div>
+            <div class="d-flex justify-content-between">
+              <div className="viewdetail">            
+        View Detail >>>
+              </div>
+              <div className="d-flex">
+                <i class="bi bi-pencil-square dashedit"></i>
+                <i class="bi bi-trash-fill dashdelete"></i>
+              </div>
+            </div>
+
+          </div>
+          <div class=" col-3 shadow p-3 mb-5 bg-body-tertiary rounded">
+            <div class="d-flex justify-content-between">
+              <div>
+                <img src={Group}></img>
+              </div>
+              <div className="pending">
+                Pending From Operations
+              </div>
+            </div>
+            <div>
+              <span className="dashhead"> Job ID:</span>
+              <p className="toms">TOMS/OM/24/123</p>
+            </div>
+            <div>
+              <span className="dashhead"> Port Name:</span>
+              <p className="toms">Sohar Port</p>
+            </div>
+            <div>
+              <span className="dashhead"> Vessel Name:</span>
+              <p className="toms">MV Viva Globus</p>
+            </div>
+            <div class="d-flex justify-content-between">
+              <div className="viewdetail">            
+        View Detail >>>
+              </div>
+              <div className="d-flex">
+                <i class="bi bi-pencil-square dashedit"></i>
+                <i class="bi bi-trash-fill dashdelete"></i>
+              </div>
+            </div>
+
+          </div>
+          <div class=" col-3 shadow p-3 mb-5 bg-body-tertiary rounded">
+            <div class="d-flex justify-content-between">
+              <div>
+                <img src={Group}></img>
+              </div>
+              <div className="Operations">
+                Operations Completed
+              </div>
+            </div>
+            <div>
+              <span className="dashhead"> Job ID:</span>
+              <p className="toms">TOMS/OM/24/123</p>
+            </div>
+            <div>
+              <span className="dashhead"> Port Name:</span>
+              <p className="toms">Sohar Port</p>
+            </div>
+            <div>
+              <span className="dashhead"> Vessel Name:</span>
+              <p className="toms">MV Viva Globus</p>
+            </div>
+            <div class="d-flex justify-content-between">
+              <div className="viewdetail">            
+        View Detail >>>
+              </div>
+              <div className="d-flex">
+                <i class="bi bi-pencil-square dashedit"></i>
+                <i class="bi bi-trash-fill dashdelete"></i>
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
     </>
   );
