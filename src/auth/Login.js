864a64cfd945f36eafef1930334b82ba6bfa1b14
ratheesh-srishtis <113422448+ratheesh-srishtis@users.noreply.github.com>
2024-12-24 09:30:47 +0530

    version-change

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -269,7 +269,7 @@ const Login = () => {
           </p>
         </div>
         <div className="d-flex justify-content-center loginpageversion">
-          Version: 0.020
+          Version: 0.021
         </div>
       </div>
 
