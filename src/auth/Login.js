d2f2100a39104d46fc34f701b694170d1b3cb856
ratheesh-srishtis <113422448+ratheesh-srishtis@users.noreply.github.com>
2025-01-01 21:01:40 +0530

    bug-fixes

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -269,7 +269,7 @@ const Login = () => {
           </p>
         </div>
         <div className="d-flex justify-content-center loginpageversion">
-          Version: 0.027
+          Version: 0.028
         </div>
       </div>
 
--- src/pages/Quotations.js
+++ src/pages/Quotations.js
@@ -387,7 +387,7 @@ const Quotations = ({ loginResponse }) => {
                 },
               }}
               pagination // Enables pagination
-              pageSizeOptions={[5, 10, 20]} // Sets available page size options
+              pageSizeOptions={[5, 10, 20, 50, 100]} // Sets available page size options
               initialState={{
                 pagination: {
                   paginationModel: {
